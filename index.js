1a21819a0fd3d0e3f3599a4e6f990b70e669079d
simonepri <simone.pri@hotmail.it>
2017-12-05 21:38:11 +0100

    Fix JSDoc

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const getMap = require('@geo-maps/earth-seas-1m');
 let landLookup = null;
 
 /**
- * Returns wheather the given point is land or not.
+ * Returns wheather the given point is in the sea or not.
  * @public
  * @param {number} lat  The latitude of the point.
  * @param {number} lng  The longitude of the point.
- * @return {boolean} True if the point is on land, false otherwise.
+ * @return {boolean} True if the point is in the sea, false otherwise.
  */
 function isSea(lat, lng) {
   if (landLookup === null) {

c45824400cf7cb7865511bcd8fe2b7b0a61d22e0
simonepri <simone.pri@hotmail.it>
2017-12-05 20:20:45 +0100

    Add demo

---
+++ demo.js
@@ -0,0 +1,39 @@
+const leaflet = require('leaflet');
+const GeoJsonLookup = require('geojson-geometries-lookup');
+
+async function fetchMapAsync() {
+  const response = await fetch('https://unpkg.com/@geo-maps/earth-lands-10m/map.geo.json');
+  const data = await response.json();
+  return data;
+}
+
+let landLookup = null;
+async function isLand(lat, lng) {
+  if (landLookup === null) {
+    const map = await fetchMapAsync();
+    landLookup = new GeoJsonLookup(map);
+  }
+  return landLookup.hasContainers({type: 'Point', coordinates: [lng, lat]});
+}
+
+async function showInfo(map, latlng) {
+  latlng = latlng.wrap();
+
+  const output = {
+    isLand: await isLand(latlng.lat, latlng.lng)
+  };
+
+  const msg = JSON.stringify(output, null, 2).replace(/\r?\n|\r/g, '<br/>').replace(/ /g, '&ensp;');
+  map.openPopup(msg, latlng);
+}
+
+const map = leaflet.map('map').setView([0, 0], 1);
+leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+map.on('click', e => {
+  const latlng = e.latlng;
+  map.openPopup('Loading... ⌛️', latlng);
+
+  setTimeout(() => showInfo(map, latlng), 0);
+});

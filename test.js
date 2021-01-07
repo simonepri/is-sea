add02234cf24a0fb695751f56700a5af040a21f2
contra <yo@contra.io>
2021-01-07 10:31:00 -0700

    chore: update repo

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import test from 'ava';
 
-import m from '.';
+import m from './index.js';
 
 test('41.8904617N, 12.4746088W should return false for a point in an island', t => {
   t.false(m(41.8904617, 12.4746088));
 });
 
 test('41.890007N, 12.477570W should return false for a point in the river', t => {
-  t.false(m(41.890007, 12.477570));
+  t.false(m(41.890007, 12.47757));
 });
 
 test('42.105019N, 12.316765W should return false for a point in the lake', t => {

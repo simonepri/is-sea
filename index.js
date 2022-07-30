771de9eeec351f8ee3a8c22f55484f8e4be9d928
Lee Prevost <lee@prevost.net>
2022-07-30 10:49:23 -0400

    updated comparison to allow for negative radius

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ let landLookup = null;
  * @public
  * @param {number} lat  The latitude of the point.
  * @param {number} lng  The longitude of the point.
- * @param {radius}, optional, distance to draw the buffer around/within earth-seas boundary.   12 nautical miles is territorial boundary.   https://en.wikipedia.org/wiki/Territorial_waters
+ * @param {radius}, optional, distance to draw the buffer around/within earth-seas boundary (allows neg number).   12 nautical miles is territorial boundary.   
+ * https://en.wikipedia.org/wiki/Territorial_waters
  * @param {units}, valid units: default is 'nauticalmiles'.  accepted units are defind by turf module convert Length http://turfjs.org/docs/#convertLength 
  * @return {boolean} True if the point is in the sea, false otherwise.
  */
 function isSea(lat, lng, radius=0, units='nauticalmiles') {
   if (landLookup === null) {
     const map = getMap();
-    if buffer > 0 {
+    if buffer != 0 {
       map = buffer(map, radius, {units: units})
     }
     landLookup = new GeoJsonLookup(map);

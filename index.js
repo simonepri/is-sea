5f4e21996af44cd4164b1a68d5b30f5515ee0575
Lee Prevost <lee@prevost.net>
2022-07-30 10:14:17 -0400

    added buffer logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const GeoJsonLookup = require('geojson-geometries-lookup');
 const getMap = require('@geo-maps/earth-seas-10m');
+const buffer = require('@turf/buffer');
 
 let landLookup = null;
 
@@ -10,11 +11,16 @@ let landLookup = null;
  * @public
  * @param {number} lat  The latitude of the point.
  * @param {number} lng  The longitude of the point.
+ * @param {radius}, optional, distance to draw the buffer around/within earth-seas boundary.   12 nautical miles is territorial boundary.   https://en.wikipedia.org/wiki/Territorial_waters
+ * @param {units}, valid units: default is kilometers.  typical is nauticalmiles http://turfjs.org/docs/#convertLength 
  * @return {boolean} True if the point is in the sea, false otherwise.
  */
-function isSea(lat, lng) {
+function isSea(lat, lng, radius=0, units='nauticalmiles') {
   if (landLookup === null) {
     const map = getMap();
+    if buffer > 0 {
+      map = buffer(map, radius, {units: units})
+    }
     landLookup = new GeoJsonLookup(map);
   }
 

261cc6dd4d43418248c100c666ae3409bcd2efdf
Lee Prevost <lee@prevost.net>
2022-07-30 10:39:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let landLookup = null;
  * @param {number} lat  The latitude of the point.
  * @param {number} lng  The longitude of the point.
  * @param {radius}, optional, distance to draw the buffer around/within earth-seas boundary.   12 nautical miles is territorial boundary.   https://en.wikipedia.org/wiki/Territorial_waters
- * @param {units}, valid units: default is kilometers.  typical is nauticalmiles http://turfjs.org/docs/#convertLength 
+ * @param {units}, valid units: default is 'nauticalmiles'.  accepted units are defind by turf module convert Length http://turfjs.org/docs/#convertLength 
  * @return {boolean} True if the point is in the sea, false otherwise.
  */
 function isSea(lat, lng, radius=0, units='nauticalmiles') {

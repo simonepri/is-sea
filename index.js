36b4349d2734ca1c53517f3c4eb3fc4fb7c139a7
Lee Prevost <lee@prevost.net>
2022-08-07 20:22:00 -0400

    Refactoring -- using countries-maritime-10m

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const GeoJsonLookup = require('geojson-geometries-lookup');
-const getMap = require('@geo-maps/earth-seas-10m');
-const buffer = require('@turf/buffer');
+const getMap = require('@geo-maps/countries-maritime-10m');  //changed to maritime sea boundaries.  May need to negate boolean response of hasContainers method.
 
 let landLookup = null;
 
@@ -11,16 +10,11 @@ let landLookup = null;
  * @public
  * @param {number} lat  The latitude of the point.
  * @param {number} lng  The longitude of the point.
- * @param {radius}, optional, distance to draw the buffer around/within earth-seas boundary.   12 nautical miles is territorial boundary.   https://en.wikipedia.org/wiki/Territorial_waters
- * @param {units}, valid units: default is 'nauticalmiles'.  accepted units are defind by turf module convert Length http://turfjs.org/docs/#convertLength 
  * @return {boolean} True if the point is in the sea, false otherwise.
  */
 function isSea(lat, lng, radius=0, units='nauticalmiles') {
   if (landLookup === null) {
     const map = getMap();
-    if buffer > 0 {
-      map = buffer(map, radius, {units: units})
-    }
     landLookup = new GeoJsonLookup(map);
   }
 
